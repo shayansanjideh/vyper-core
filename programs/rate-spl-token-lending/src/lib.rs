1188a54b3edf371cd4a503c0f5677b31a45e3464
shayansanjideh <94887104+shayansanjideh@users.noreply.github.com>
2022-06-30 22:40:27 -0400

    added skeleton

--- programs/rate-spl-token-lending/src/lib.rs
+++ programs/rate-spl-token-lending/src/lib.rs
@@ -1,15 +1,48 @@
+// Provide fair value for a particular Solend pool (mono-asset)
+
+// Step 1. Read Solend data
+// Step 2. Determine fair value of tokens
+// Step 3. Implement rate state plugin (RateState)
+
+// TODO: Fix `Reserve` struct import, add additional imports
+// https://github.com/solana-labs/solana-program-library/blob/master/token-lending/program/src/state/reserve.rs
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::token_lending::state::Reserve; // ?
+use spl_token_lending::state::Reserve; // ?
+use crate::refresh_tranche_fair_value::{RateState, RefreshTrancheFairValue}; // ?
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-#[program]
-pub mod rate_spl_token_lending {
-    use super::*;
+// TODO: Read data from Solend (or other SPL-based lending platforms)
+// Initialize vault data from Solend
+// Borrowed from castle-vault/src/state.rs
+#[account]
+pub struct Vault {
+    /// Program version when initialized: [major, minor, patch]
+    pub version: [u8; 3],
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    /// Account that is allowed to call the subsequent instructions
+    pub owner: Pubkey,
+
+    // TODO: add other fields as necessary?
+}
+
+// TODO: Determine fair value of tokens
+// First, deserialize the data
+// Borrowed from castle-vault/adapters/solend.rs
+// TODO: Fix Reserve struct import
+#[derive(Clone)]
+pub struct SolendReserve(Reserve);
+
+impl anchor_lang::AccountDeserialize for SolendReserve {
+    fn try_deserialize(buf: &mut &[u8]) -> Result<Self, ProgramError> {
+        SolendReserve::try_deserialize_unchecked(buf)
+    }
+
+    fn try_deserialize_unchecked(buf: &mut &[u8]) -> Result<Self, ProgramError> {
+        <Reserve as solana_program::program_pack::Pack>::unpack(buf).map(SolendReserve)
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+// TODO: Collect prices from deserialized token values
+
+// TODO: Implement RateState plugin
